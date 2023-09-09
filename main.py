d172038d619688a89c05ca8bff1f10d274838461
OksanaFedyukova <avseniya222@gmail.com>
2023-09-09 20:20:06 +0200

    complex number

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print()
+
+complex_one = 7 + 2j
+complex_two = 4 + 3j
+
+sum = complex_one + complex_two
+
+print(sum)
+
+print(type(sum))
+
+mult = complex_one * complex_two
+
+print(mult)
+#sum = (7+2j)*(4+3j) = 28 + 21j +8j +6j^2
+##sum = (7+2j)*(4+3j) = 28 + 21j +8j -6 
